2c26201831f4bb70dc8558f2f02d9d3627c14012
bolee10499-png <bolee10499@gmail.com>
2025-11-22 22:56:22 -0500

    Delete blender directory

--- blender/cosmic_tesseract.py
+++
@@ -1,7 +0,0 @@
-# COSMIC_TESSERACT_TOPOLOGY.py
-# ...existing code...
-
-# Save render to tesseract_render.png for Discord bot automation
-import bpy
-bpy.context.scene.render.filepath = r"C:\html-project\tesseract_render.png"
-bpy.ops.render.render(write_still=True)
